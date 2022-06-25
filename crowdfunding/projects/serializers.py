4e16cf3232a2bf16df9c72024e52c74be89939d8
Miao Zhao <miao_zhao@yahoo.com>
2022-06-25 12:03:41 +1000

    Split the project serializer into two to reduce the data fetching when viewing all projects

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -25,7 +25,11 @@ class ProjectSerializer(serializers.Serializer):
     is_open = serializers.BooleanField()
     date_created = serializers.DateTimeField()
     owner = serializers.CharField(max_length=200)
-    pledges = PledgeSerializer(many=True, read_only=True)
+    # pledges = PledgeSerializer(many=True, read_only=True)
 
     def create(self, validated_data):
         return Project.objects.create(**validated_data)
+
+
+class ProjectDetailSerializer(ProjectSerializer):
+    pledges = PledgeSerializer(many=True, read_only=True)
