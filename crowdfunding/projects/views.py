2766e8701434fceeb8d258c1746c4a55db0afb69
Miao Zhao <miao_zhao@yahoo.com>
2022-06-25 11:01:45 +1000

    Create Post function in Projects' views

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -10,3 +10,11 @@ class ProjectList(APIView):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
