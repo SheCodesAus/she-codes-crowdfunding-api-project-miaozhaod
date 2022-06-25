e663ac21b1a3f299154b30e2447dc53230a3707c
Miao Zhao <miao_zhao@yahoo.com>
2022-06-25 11:55:21 +1000

    Update urls.py

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -1,4 +1,3 @@
-from urllib.parse import urlparse
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
@@ -6,6 +5,7 @@ from . import views
 urlpatterns = [
     path("projects/", views.ProjectList.as_view()),
     path("projects/<int:pk>", views.ProjectDetail.as_view()),
+    path("pledges/", views.PledgeList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
