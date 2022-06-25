5f664aa8314ae8ef5149b7deac94d8b217523425
Miao Zhao <miao_zhao@yahoo.com>
2022-06-25 10:26:03 +1000

    Create projects' urls

---
+++ crowdfunding/projects/urls.py
@@ -0,0 +1,10 @@
+from urllib.parse import urlparse
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    path("projects/", views.ProjectList.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
