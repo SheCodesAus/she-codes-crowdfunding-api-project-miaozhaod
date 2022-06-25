9c7f787e5bcef8a1e8dfffbb96a24ca5ef33df9b
Miao Zhao <miao_zhao@yahoo.com>
2022-06-25 12:17:59 +1000

    Update view to reflect changes in split serializers

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project, Pledge
-from .serializers import ProjectSerializer, PledgeSerializer
+from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
 from django.http import Http404
 from rest_framework import status
 
@@ -44,5 +44,5 @@ class ProjectDetail(APIView):
 
     def get(self, request, pk):
         project = self.get_object(pk)
-        serializer = ProjectSerializer(project)
+        serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
