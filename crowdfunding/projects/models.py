cd5ebd2676d575a02fb84f0cbb026db4e4e847fa
Miao Zhao <miao_zhao@yahoo.com>
2022-06-28 18:16:10 +1000

    Update model of `projects` with the created user for owner and supporter

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 class Pledge(models.Model):
     amount = models.IntegerField()
     comment = models.CharField(max_length=200)
     anonymous = models.BooleanField()
     project = models.ForeignKey("Project", on_delete=models.CASCADE, related_name="pledges")
-    supporter = models.CharField(max_length=200)
+    # supporter = models.CharField(max_length=200)
+    supporter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="supporters_pledges")
 
 
 class Project(models.Model):
@@ -18,4 +19,5 @@ class Project(models.Model):
     date_due = models.DateTimeField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField()
-    owner = models.CharField(max_length=200)
+    # owner = models.CharField(max_length=200)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="owner_projects")
