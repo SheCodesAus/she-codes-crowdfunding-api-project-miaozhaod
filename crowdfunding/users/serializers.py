0452e830a6f02f42711b29b978de196c9451f3c2
Miao Zhao <miao_zhao@yahoo.com>
2022-07-02 12:38:53 +1000

    Update users' `serializers` to save and hash password

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth.hashers import make_password
 
 class CustomUserSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     username = serializers.CharField(max_length=200)
     email = serializers.CharField(max_length=200)
+    bio = serializers.CharField(max_length=500)
+    avatar = serializers.URLField()
+    password = serializers.CharField(style={'input_type': 'password'})
+
 
     def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
         return CustomUser.objects.create(**validated_data)
